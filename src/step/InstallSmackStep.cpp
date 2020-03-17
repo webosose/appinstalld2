7eeaff8fda1aadfec347326c6c7146a9a63086bd
Myungchul Kim <myungchul.kim@lge.com>
2020-03-17 14:32:49 +0900

    Support SMACK labels & rules generation
    Release Notes:
    Support SMACK labels & rules generation
    
    :Detailed Notes:
    Add InstallSmackStep and RemoveSmackStep
    
    :Testing Performed:
    QWA-1736
    QWA-2325
    
    :QA Notes:
    
    :Issues Addressed:
    [PLAT-102435] [Webos Auto]:Status is showing Stop instead of foreground, when we launch the com.webos.app.sam-qml app
    [PLAT-102361] [Webos Auto]: Hello, QML application! is not displayed on the screen
    
    Change-Id: I15675d81818919f58d6501e014de4c4ffc7033a9

---
+++ src/step/InstallSmackStep.cpp
@@ -0,0 +1,178 @@
+// Copyright (c) 2020 LG Electronics, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#include "InstallSmackStep.h"
+
+#include "base/Logging.h"
+#include "installer/AppInfo.h"
+#include "installer/InstallHistory.h"
+#include "installer/Task.h"
+#include "settings/Settings.h"
+#include "settings/Smack.h"
+
+InstallSmackStep::InstallSmackStep()
+{
+}
+
+InstallSmackStep::~InstallSmackStep()
+{
+    LOG_DEBUG("InstallSmackStep::~InstallSmackStep() called\n");
+}
+
+bool InstallSmackStep::proceed(Task *task) {
+    LOG_DEBUG("InstallSmackStep::~proceed() called\n");
+
+    m_parentTask = task;
+    std::string appId = m_parentTask->getAppId();
+    std::string applicationPath = m_parentTask->getInstallBasePath() + Settings::instance().getApplicationInstallPath() + "/" + appId;
+    std::string label;
+    AppInfo appInfo(applicationPath);
+
+    GSpawnFlags flags = (GSpawnFlags)(G_SPAWN_SEARCH_PATH);
+    gchar * argv[9] = {0};
+    GError * gerr = NULL;
+    gint exit_status = 0;
+    gboolean resultStatus;
+    int index = 0;
+
+    /* Skip SMACK labeling if app is stub or smack off */
+    if (appInfo.isStub()) {
+        m_parentTask->setStep(InstallSmackComplete);
+        m_parentTask->proceed();
+        return true;
+    }
+
+    if (appInfo.isWeb())
+        label = SMACK_EXEC_WEB;
+    else
+        label = SMACK_EXEC_PREFFIX + appId;
+
+    argv[index++] = (gchar *)CHSMACK_EXEC;
+    argv[index++] = (gchar *)"-r";
+    argv[index++] = (gchar *)"-t";
+    argv[index++] = (gchar *)"-a";
+    argv[index++] = (gchar *)label.c_str();
+    if (appInfo.isNative()) {
+        argv[index++] = (gchar *)"-e";
+        argv[index++] = (gchar *)label.c_str();
+    }
+    argv[index++] = (gchar *)applicationPath.c_str();
+    argv[index++] = NULL;
+
+    LOG_DEBUG("Executing %s %s %s %s %s %s", argv[0], argv[1], argv[2], argv[3], argv[4], argv[5]);
+    resultStatus = g_spawn_sync(NULL,
+                                argv,
+                                NULL,
+                                flags,
+                                NULL,
+                                NULL,
+                                NULL,
+                                NULL,
+                                &exit_status,
+                                &gerr);
+    LOG_DEBUG("chsmack result status is %d, exit status is %d", (int)resultStatus, (int)WEXITSTATUS(exit_status));
+
+    if (gerr) {
+        LOG_ERROR(MSGID_INSTALL_SMACK_FAIL, 2,
+                  PMLOGKS(REASON, "Failed to execute chsmack"),
+                  PMLOGKS(LOGKEY_ERRTEXT,gerr->message),
+                  " ");
+        g_error_free(gerr);
+    }
+
+    if (!resultStatus || exit_status) {
+        LOG_DEBUG("Non-zero exit code from chsmack.");
+        m_parentTask->setError(ErrorInstall, APP_INSTALL_ERR_SMACK , "unable to execute chsmack command");
+        m_parentTask->proceed();
+        return false;
+    }
+
+    std::string rulesFilePath = SMACK_RULES_DIR + appId;
+    if (!appInfo.isWeb() && !g_file_test(rulesFilePath.c_str(), G_FILE_TEST_IS_REGULAR)) {
+        g_mkdir_with_parents(SMACK_RULES_DIR, 0755);
+        index = 0;
+        argv[index++] = (gchar *)SMACK_RULES_GEN_EXEC;
+        argv[index++] = (gchar *)"-b";
+        argv[index++] = (gchar *)appId.c_str();
+        argv[index++] = (gchar *)"-o";
+        argv[index++] = (gchar *)rulesFilePath.c_str();
+        argv[index++] = NULL;
+        LOG_DEBUG("Executing %s %s %s %s %s", argv[0], argv[1], argv[2], argv[3], argv[4]);
+        resultStatus = g_spawn_sync(NULL,
+                                    argv,
+                                    NULL,
+                                    flags,
+                                    NULL,
+                                    NULL,
+                                    NULL,
+                                    NULL,
+                                    &exit_status,
+                                    &gerr);
+        LOG_DEBUG("smack_rules_gen result status is %d, exit status is %d", (int)resultStatus, (int)WEXITSTATUS(exit_status));
+
+        if (gerr) {
+            LOG_ERROR(MSGID_INSTALL_SMACK_FAIL, 2,
+                      PMLOGKS(REASON, "Failed to execute smack_rule_gen"),
+                      PMLOGKS(LOGKEY_ERRTEXT,gerr->message),
+                      " ");
+            g_error_free(gerr);
+        }
+
+        if (!resultStatus || exit_status) {
+            LOG_DEBUG("Non-zero exit code from smack rules generator.");
+            m_parentTask->setError(ErrorInstall, APP_INSTALL_ERR_SMACK , "unable to execute smack_rules_gen command");
+            m_parentTask->proceed();
+            return false;
+        }
+
+        index = 0;
+        argv[index++] = (gchar *)SMACKCTL_EXEC;
+        argv[index++] = (gchar *)"apply";
+        argv[index++] = NULL;
+        LOG_DEBUG("Executing %s %s", argv[0], argv[1]);
+        resultStatus = g_spawn_sync(NULL,
+                                    argv,
+                                    NULL,
+                                    flags,
+                                    NULL,
+                                    NULL,
+                                    NULL,
+                                    NULL,
+                                    &exit_status,
+                                    &gerr);
+        LOG_DEBUG("smackctl result status is %d, exit status is %d", (int)resultStatus, (int)WEXITSTATUS(exit_status));
+
+        if (gerr) {
+            LOG_ERROR(MSGID_INSTALL_SMACK_FAIL, 2,
+                      PMLOGKS(REASON, "Failed to execute smackctl"),
+                      PMLOGKS(LOGKEY_ERRTEXT,gerr->message),
+                      "");
+            g_error_free(gerr);
+        }
+
+        if (!resultStatus || exit_status) {
+            LOG_DEBUG("Non-zero exit code from smackctl.");
+            m_parentTask->setError(ErrorInstall, APP_INSTALL_ERR_SMACK, "unable to execute smackctl command");
+            m_parentTask->proceed();
+            return false;
+        }
+    }
+
+    m_parentTask->setStep(InstallSmackComplete);
+    m_parentTask->proceed();
+    return true;
+}
+
